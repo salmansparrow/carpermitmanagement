dc68830dea1b13ac46046fb006d15c4dfc2b8e43
salman <truesalman7@gmail.com>
2025-02-15 14:16:17 +0500

    database connected

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+import dbConnect from "../../../lib/dbConnect";
+
+export default async function handler(req, res) {
+  await dbConnect();
+  console.log(dbConnect);
+  res.status(200).json({ message: "database connected" });
 }
